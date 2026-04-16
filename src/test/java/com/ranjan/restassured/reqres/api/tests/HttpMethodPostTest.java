ba26c9c5ac72ac1bee2d7dfb2b0a87bc45018d96
Ranjan Patnaik <Ranjan.Patnaik@momentum.co.za>
2026-04-16 20:26:21 +0200

    Added Post method tests

--- src/test/java/com/ranjan/restassured/reqres/api/tests/HttpMethodPostTest.java
+++ src/test/java/com/ranjan/restassured/reqres/api/tests/HttpMethodPostTest.java
@@ -5,14 +5,13 @@ import org.testng.annotations.Test;
 import java.util.HashMap;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class HttpMethodPostTest extends BaseTest {
 
     @Test
-    public void createUser_expectHttpStatus201() {
-        HashMap<String, String> requestBody = new HashMap<String, String>();
-        requestBody.put("name", "morpheus");
-        requestBody.put("job", "dream king");
+    public void createUser_shouldReturn201() {
+        String requestBody = "{\"name\":\"morpheus\", \"job\":\"dream king\"}";
 
         given().
             header("x-api-key", "reqres_4c443c2fe563455e98d87adecc280679").
@@ -21,7 +20,27 @@ public class HttpMethodPostTest extends BaseTest {
         when().
             post("https://reqres.in/api/users").
         then().
-            statusCode(201);
+            statusCode(201).
+            body("name", equalTo("morpheus")).
+            body("job", equalTo("dream king")).
+            body("id", notNullValue()).
+            body("createdAt", notNullValue());
     }
 
+    @Test
+    public void createUser_withValidPayload_shouldReturn201AndGenerateId() {
+        String requestBody = "{\"name\":\"neo\", \"job\":\"the one\"}";
+
+        given().
+            header("x-api-key", "reqres_4c443c2fe563455e98d87adecc280679").
+            contentType("application/json").
+            body(requestBody).
+        when().
+            post("/api/users").
+        then().
+            statusCode(201).
+            body("name", equalTo("neo")).
+            body("job", equalTo("the one")).
+            body("id", notNullValue());
+    }
 }
